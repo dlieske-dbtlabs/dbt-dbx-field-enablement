758a9ffa5c714a0bdd119d15bd11114dda97013f
Dustin Lieske <dustin.lieske@dbtlabs.com>
2026-07-20 21:52:35 +0000

    Outdated SL spec refactoring and enhancements

--- platform/models/intermediate/int_order_items_enriched.sql
+++ platform/models/intermediate/int_order_items_enriched.sql
@@ -1,5 +1,11 @@
+{{ config(materialized='view') }}
+
+
 with order_items as (
     select * from {{ ref('stg_order_items') }}
+),
+orders as (
+    select * from {{ ref('stg_orders') }}
 ),
 products as (
     select * from {{ ref('stg_products') }}
@@ -8,6 +14,9 @@ enriched as (
     select
         oi.order_item_id,
         oi.order_id,
+        o.order_date,
+        o.status,
+        o.customer_id,
         oi.product_id,
         p.product_name,
         p.category,
@@ -15,6 +24,8 @@ enriched as (
         oi.unit_price,
         oi.line_total
     from order_items oi
+    left join orders o on oi.order_id = o.order_id
     left join products p on oi.product_id = p.product_id
 )
 select * from enriched
+
